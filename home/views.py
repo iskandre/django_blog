443569c82dc669891f6c8283ad2ecbdbed2da985
iskandre <iskandre@yahoo.co.ukm>
2018-05-27 23:18:44 +0200

    commiting#

--- home/views.py
+++ home/views.py
@@ -44,8 +44,6 @@ def post(request):
                     break
     
     temp_dict = {}   
-    print('resDict_natalie %s' %resDict_natalie)
-    print('resDict_iskandre %s' %resDict_iskandre)
     for item in resDict_natalie['root_users_dict'].keys():
         temp_dict[item] = resDict_natalie['root_users_dict'][item]
         if len(temp_dict) > 7:
@@ -55,16 +53,16 @@ def post(request):
 
 #    temp_dict = resDict_natalie['root_users_dict']
 #    resDict_iskandre_natalie['root_users_dict'] = take(10, temp_dict.items())
-    resDict_iskandre['correlation_heatmap_graph'] = '/root/website/instagram_tool_project/cached_data/' + 'iskandre' + '/' + resDict_iskandre['correlation_heatmap_graph'].split('/')[len(resDict_iskandre['correlation_heatmap_graph'].split('/')) - 1]
+    resDict_iskandre['correlation_heatmap_graph'] = dir_path + '/cached_data/' + 'iskandre' + '/' + resDict_iskandre['correlation_heatmap_graph'].split('/')[len(resDict_iskandre['correlation_heatmap_graph'].split('/')) - 1]
     print('correlation_heatmap_graph updated %s' %resDict_iskandre['correlation_heatmap_graph'])
-    resDict_iskandre['pairplot_homeCountry_graph'] = '/root/website/instagram_tool_project/cached_data/' + 'iskandre' + '/' + resDict_iskandre['pairplot_homeCountry_graph'].split('/')[len(resDict_iskandre['pairplot_homeCountry_graph'].split('/')) - 1]
-    resDict_iskandre['pairplot_main_graph'] = '/root/website/instagram_tool_project/cached_data/' + 'iskandre' + '/' + resDict_iskandre['pairplot_main_graph'].split('/')[len(resDict_iskandre['pairplot_main_graph'].split('/')) - 1]
-    resDict_iskandre['pairplot_followed_by'] = '/root/website/instagram_tool_project/cached_data/' + 'iskandre' + '/' + resDict_iskandre['pairplot_followed_by'].split('/')[len(resDict_iskandre['pairplot_followed_by'].split('/')) - 1]
-    resDict_iskandre['pairplot_follows'] = '/root/website/instagram_tool_project/cached_data/' + 'iskandre' + '/' + resDict_iskandre['pairplot_follows'].split('/')[len(resDict_iskandre['pairplot_follows'].split('/')) - 1]
-    resDict_iskandre['pairplot_posts_count'] = '/root/website/instagram_tool_project/cached_data/' + 'iskandre' + '/' + resDict_iskandre['pairplot_posts_count'].split('/')[len(resDict_iskandre['pairplot_posts_count'].split('/')) - 1]
-    resDict_iskandre['pairplot_countriesCount'] = '/root/website/instagram_tool_project/cached_data/' + 'iskandre' + '/' + resDict_iskandre['pairplot_countriesCount'].split('/')[len(resDict_iskandre['pairplot_countriesCount'].split('/')) - 1]
-    resDict_iskandre['pairplot_avgPostingFreq'] = '/root/website/instagram_tool_project/cached_data/' + 'iskandre' + '/' + resDict_iskandre['pairplot_avgPostingFreq'].split('/')[len(resDict_iskandre['pairplot_avgPostingFreq'].split('/')) - 1]
-    resDict_iskandre['pairplot_withGeo'] = '/root/website/instagram_tool_project/cached_data/' + 'iskandre' + '/' + resDict_iskandre['pairplot_withGeo'].split('/')[len(resDict_iskandre['pairplot_withGeo'].split('/')) - 1]
+    resDict_iskandre['pairplot_homeCountry_graph'] = dir_path + '/cached_data/' + 'iskandre' + '/' + resDict_iskandre['pairplot_homeCountry_graph'].split('/')[len(resDict_iskandre['pairplot_homeCountry_graph'].split('/')) - 1]
+    resDict_iskandre['pairplot_main_graph'] = dir_path + '/cached_data/' + 'iskandre' + '/' + resDict_iskandre['pairplot_main_graph'].split('/')[len(resDict_iskandre['pairplot_main_graph'].split('/')) - 1]
+    resDict_iskandre['pairplot_followed_by'] = dir_path + '/cached_data/' + 'iskandre' + '/' + resDict_iskandre['pairplot_followed_by'].split('/')[len(resDict_iskandre['pairplot_followed_by'].split('/')) - 1]
+    resDict_iskandre['pairplot_follows'] = dir_path + '/cached_data/' + 'iskandre' + '/' + resDict_iskandre['pairplot_follows'].split('/')[len(resDict_iskandre['pairplot_follows'].split('/')) - 1]
+    resDict_iskandre['pairplot_posts_count'] = dir_path + '/cached_data/' + 'iskandre' + '/' + resDict_iskandre['pairplot_posts_count'].split('/')[len(resDict_iskandre['pairplot_posts_count'].split('/')) - 1]
+    resDict_iskandre['pairplot_countriesCount'] = dir_path + '/cached_data/' + 'iskandre' + '/' + resDict_iskandre['pairplot_countriesCount'].split('/')[len(resDict_iskandre['pairplot_countriesCount'].split('/')) - 1]
+    resDict_iskandre['pairplot_avgPostingFreq'] = dir_path + '/cached_data/' + 'iskandre' + '/' + resDict_iskandre['pairplot_avgPostingFreq'].split('/')[len(resDict_iskandre['pairplot_avgPostingFreq'].split('/')) - 1]
+    resDict_iskandre['pairplot_withGeo'] = dir_path + '/cached_data/' + 'iskandre' + '/' + resDict_iskandre['pairplot_withGeo'].split('/')[len(resDict_iskandre['pairplot_withGeo'].split('/')) - 1]
                 
     return render(request,'home/analysis_post.htm',{'followerCTR_map':resDict_iskandre['followerCTR_map'],'sum_followers_map':resDict_iskandre['sum_followers_map'],
                                                  'results_table':temp_dict,'clusters_string':resDict_natalie['stringJson'],
@@ -75,14 +73,14 @@ def post(request):
                                     'pairplot_followed_by':resDict_iskandre['pairplot_followed_by'],
                                     'pairplot_follows':resDict_iskandre['pairplot_follows'],'pairplot_posts_count':resDict_iskandre['pairplot_posts_count'],
                                     'pairplot_countriesCount':resDict_iskandre['pairplot_countriesCount'],'pairplot_avgPostingFreq':resDict_iskandre['pairplot_avgPostingFreq'],
-                                    'df_userMapScreen':'/root/website/instagram_tool_project/cached_data/iskandre/dfUserMap_example.png',
-                                    'bugsModelScreen':'/root/website/instagram_tool_project/cached_data/iskandre/bugs_model_screenshot.png',
+                                    'df_userMapScreen':dir_path + '/cached_data/iskandre/dfUserMap_example.png',
+                                    'bugsModelScreen':dir_path + '/cached_data/iskandre/bugs_model_screenshot.png',
                                     'pairplot_withGeo':resDict_iskandre['pairplot_withGeo'],
-                                    'b1':'/cached_data/iskandre/b1.png','b2':'/root/website/instagram_tool_project/cached_data/iskandre/b2.png',
-                                    'b3':'/cached_data/iskandre/b3.png','b4':'/root/website/instagram_tool_project/cached_data/iskandre/b4.png',
-                                    'b5':'/cached_data/iskandre/b5.png','b6':'/root/website/instagram_tool_project/cached_data/iskandre/b6.png',
-                                    'b7':'/cached_data/iskandre/b7.png','b8':'/root/website/instagram_tool_project/cached_data/iskandre/b8.png',
-                                    'b9':'/cached_data/iskandre/b9.png','b10':'/root/website/instagram_tool_project/cached_data/iskandre/b10.png'
+                                    'b1':'/cached_data/iskandre/b1.png','b2':dir_path + '/cached_data/iskandre/b2.png',
+                                    'b3':'/cached_data/iskandre/b3.png','b4':dir_path + '/cached_data/iskandre/b4.png',
+                                    'b5':'/cached_data/iskandre/b5.png','b6':dir_path + '/cached_data/iskandre/b6.png',
+                                    'b7':'/cached_data/iskandre/b7.png','b8':dir_path + '/cached_data/iskandre/b8.png',
+                                    'b9':'/cached_data/iskandre/b9.png','b10':dir_path + '/cached_data/iskandre/b10.png'
                                                  })
 
 
